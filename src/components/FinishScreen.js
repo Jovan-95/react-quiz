c0f2b614051ef4e91756a200076fff33370a855f
jovan <vuksanovicjovan@hotmail.com>
2025-02-07 14:15:11 +0100

    Initial commit for React Quiz project

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,28 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "Great";
+  if (percentage >= 80 && percentage < 100) emoji = "Very good";
+  if (percentage >= 50 && percentage < 80) emoji = "Good";
+  if (percentage >= 0 && percentage < 50) emoji = "Ok";
+  if (percentage === 0) emoji = "Bad";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore"> (Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
---
+++ src/components/Timer.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemainging }) {
+  const mins = Math.floor(secondsRemainging / 60);
+  const seconds = secondsRemainging % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins} : {seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
